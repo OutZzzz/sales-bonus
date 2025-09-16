071c3ce230602b14e8c2f4b0ea7238f2b4c6640a
Alexey Sudakov <outzz@yandex.ru>
2025-09-16 15:21:08 +0300

    feat: добавил Расчет выручки и прибыли для каждого продавца, Сортировку продавцов по прибыли

--- src/main.js
+++ src/main.js
@@ -7,14 +7,46 @@
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
    const { discount, sale_price, quantity } = purchase;
-   /* console.log(discount, sale_price, quantity) */
+   let disc = 1 - (discount / 100)
+   /* console.log('sale price:', sale_price, 'quantity:', quantity, 'discount:', disc) */
+   return (sale_price * quantity) * disc
 }
 
-const itemsList = data.purchase_records.forEach((record) => {
-    for (const item in record['items']) {
-        calculateSimpleRevenue(record['items'][item]);
+// @TODO: Подготовка промежуточных данных для сбора статистики
+    const sellerStats = data.sellers.map(seller => (
+    {
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
     }
-   });
+    ));
+    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    const sellerIndex = Object.fromEntries(sellerStats.map(item => [item.id, item]));
+    const productIndex = Object.fromEntries(data.products.map(item => [item.sku, item]));
+
+data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id];
+        seller.sales_count++
+        seller.revenue += record.total_amount;
+
+        record.items.forEach(item => {
+            const product = productIndex[item.sku];
+            const cost = product.purchase_price *  item.quantity;
+            const revenue = calculateSimpleRevenue(item);
+            const profit = revenue - cost
+            seller.profit += profit;
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0
+            }
+            seller.products_sold[item.sku]++
+
+        })
+    })
+sellerStats.sort()
+console.log(sellerStats)
 
 /**
  * Функция для расчета бонусов
@@ -69,10 +101,26 @@ function analyzeSalesData(data, options) {
     const sellerIndex = Object.fromEntries(sellerStats.map(item => [item.id, item]));
     const productIndex = Object.fromEntries(data.products.map(item => [item.sku, item]));
     // @TODO: Расчет выручки и прибыли для каждого продавца
+    data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id];
+        seller.sales_count++
+        seller.revenue += record.total_amount;
 
+        record.items.forEach(item => {
+            const product = productIndex[item.sku];
+            const cost = product.purchase_price *  item.quantity;
+            const revenue = calculateSimpleRevenue(item);
+            const profit = revenue - cost
+            seller.profit += profit;
+            if (!seller.products_sold[item.sku]) {
+                seller.products_sold[item.sku] = 0
+            }
+            seller.products_sold[item.sku]++
+        })
+    })
     // @TODO: Сортировка продавцов по прибыли
-
+    sellerStats.sort()
     // @TODO: Назначение премий на основе ранжирования
-
+    
     // @TODO: Подготовка итоговой коллекции с нужными полями
 }
